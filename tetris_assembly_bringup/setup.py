fa497c01f5108a60e523656516d751d01d37be94
Frank Zillmann <frank.zillmann@tum.de>
2026-06-03 00:00:55 +0200

    add launch files

--- tetris_assembly_bringup/setup.py
+++ tetris_assembly_bringup/setup.py
@@ -1,3 +1,6 @@
+from glob import glob
+import os
+
 from setuptools import find_packages, setup
 
 package_name = 'tetris_assembly_bringup'
@@ -10,6 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
