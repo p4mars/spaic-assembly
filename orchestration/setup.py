8b76d5e2cb1fd2272b2d5658c838d109578e58f4
Frank Zillmann <frank.zillmann@tum.de>
2026-06-10 18:08:21 +0200

    add default orchestrator package

---
+++ orchestration/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'orchestration'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Frank Zillmann',
+    maintainer_email='frank.zillmann@tum.de',
+    description='High-level orchestrator/planner/executor for the tetris-assembly pipeline.',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'orchestrator = orchestration.orchestrator:main',
+        ],
+    },
+)
